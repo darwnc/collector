9a96bf79089e3088be966ff16d61ff0c7bcb8a0d
qty <qty1yure@gmail.com>
2019-10-18 17:18:07 +0800

    确定二进制电子手表解决方法

--- exercises/exe_test.go
+++ exercises/exe_test.go
@@ -3,9 +3,49 @@ package exercises
 import (
 	"fmt"
 	"math"
+	"strconv"
 	"testing"
 )
 
 func TestBinaryWatch(t *testing.T) {
+	//1<<N
 	fmt.Println("pow", math.Pow(2, 4))
+	fmt.Println("combination=", combination(1, 6))
+	testSlice := make([]string, 0)
+	fmt.Println("len=", len(testSlice), "-cap=", cap(testSlice))
+	num := [...]string{"a", "b", "c"}
+	result := make([][]string, 0)
+	// fmt.Printf("return addr %#[1]v\n", len(&result[0]))
+	combinationDetail(num, 0, 3, &result)
+	fmt.Println(result)
+
+	// result0 := a(10)
+	// fmt.Printf("return addr%p %#[1]v\n", result0)
+
+	// result1 := make([]string, 0)
+	// b(10, &result1)
+	// fmt.Printf("result1 addr %p %#[1]v\n", &result1)
+}
+func a(b int) (all []string) {
+	if b == 1 {
+		all = append(all, strconv.Itoa(1))
+		return
+	}
+	if all == nil {
+		all = make([]string, 0)
+	}
+	all = append(all, strconv.Itoa(b))
+	fmt.Printf("addr %p\n", &all)
+	return append(all, a(b-1)...)
+}
+
+func b(a int, result *[]string) {
+
+	if a == 1 {
+		*result = append(*result, strconv.Itoa(1))
+		return
+	}
+	*result = append(*result, strconv.Itoa(a))
+	fmt.Printf("---addr %p\n", result)
+	b(a-1, result)
 }
--- exercises/exercises.go
+++ exercises/exercises.go
@@ -1,29 +1,56 @@
 package exercises
 
-import (
-	"math"
-)
+import "fmt"
 
 var hours = [...]float64{0, 1, 2, 3}
 var mins = [...]float64{0, 1, 2, 3, 4, 5}
+var timeTable = [...]int{1, 2, 4, 8, 1, 2, 4, 8, 16, 32}
+var indexTable = [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-//练习  n分布
-//n>6两个部分都有hours mins都有 n<=6可能有两个部分有，一个部分可能有
+//练习
+//https://leetcode-cn.com/problems/binary-watch/
+//找出indexTable给定指数的组合，去timeTable内的值即可得到，然后判断是否合适规则
+//合并成一个从里面任意取第二部分的组合[1,2,4,8,1,2,4,8,16,32] [0,0,0,0,0,0,0,0,0,0]
 func binaryWatch(num int) {
-
 	for i := 0; i < num; i++ {
 
 	}
 }
 
-//遍历每一个可能的位置位数n 代表个数，两个
-func hoursCount() {
-	for _, value := range hours {
-		math.Pow(2, value)
+//num包含个数
+func combinationDetail(all [3]string, begin, num int, result *[][]string) {
+	//总个数有num个 从第一位开始去，取到满足有num个
+	// container := [count]string{}
+	// *result = append(*result, all[begin])
+
+	count := combination(num, len(all))
+	fmt.Println("count=", count)
+	for i := int64(0); i < count; i++ {
+		var temp string
+
+		fmt.Println("index=", temp)
 	}
 }
-func minsCount() {
-	for _, value := range hours {
-		math.Pow(2, value)
+
+//计算排列的个数 n取出个数 m基数 n<=m
+func combination(n, m int) (com int64) {
+	com = 1
+	if n == 0 || m == 0 {
+		//
+		return
 	}
+	resultM := factorial(int64(m))
+	resultN := factorial(int64(n))
+	resultDiv := factorial(int64(m - n))
+	com = resultM / (resultN * resultDiv)
+	return
+}
+
+//num>0
+func factorial(num int64) (result int64) {
+	result = 1
+	for i := int64(1); i <= num; i++ {
+		result = result * int64(i)
+	}
+	return
 }
