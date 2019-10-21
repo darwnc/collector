751b1d0888490f99d130ce78bd09151a7a5e7cbe
qty <qty1yure@gmail.com>
2019-10-21 16:59:51 +0800

    plot库demo

--- collector.go
+++ collector.go
@@ -6,6 +6,10 @@ import (
 	"fmt"
 
 	"github.com/darwnc/collector/verify"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 
 	"net/http"
 
@@ -24,7 +28,6 @@ type user struct {
 func main() {
 	gin.SetMode(gin.DebugMode)
 	engine := gin.New()
-
 	store := cookie.NewStore([]byte("gcookie"))
 	engine.Use(sessions.Sessions("gsession", store))
 
@@ -54,6 +57,11 @@ func main() {
 			c.JSON(200, gin.H{"user/cosor": name, "session": sess})
 		})
 	}
+	engine.GET("/plot", plotTest)
+	engine.GET("/image", func(c *gin.Context) {
+		c.Header("Content-Type", "text/html; charset=utf-8")
+		c.String(200, `<img src="/plot"/>`)
+	})
 	engine.GET("/login", func(c *gin.Context) {
 		session := sessions.Default(c)
 		name := c.Query("name")
@@ -101,4 +109,63 @@ func main() {
 
 func userVerify(c *gin.Context) {
 
+}
+
+func plotTest(c *gin.Context) {
+	groupA := plotter.Values{20, 35, 30, 35, 27}
+	groupB := plotter.Values{25, 32, 34, 20, 25}
+	groupC := plotter.Values{12, 28, 15, 21, 8}
+
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Bar chart"
+	p.Y.Label.Text = "Heights"
+
+	w := vg.Points(20)
+
+	barsA, err := plotter.NewBarChart(groupA, w)
+	if err != nil {
+		panic(err)
+	}
+	barsA.LineStyle.Width = vg.Length(0)
+	barsA.Color = plotutil.Color(0)
+	barsA.Offset = -w
+
+	barsB, err := plotter.NewBarChart(groupB, w)
+	if err != nil {
+		panic(err)
+	}
+	barsB.LineStyle.Width = vg.Length(0)
+	barsB.Color = plotutil.Color(1)
+
+	barsC, err := plotter.NewBarChart(groupC, w)
+	if err != nil {
+		panic(err)
+	}
+	barsC.LineStyle.Width = vg.Length(0)
+	barsC.Color = plotutil.Color(2)
+	barsC.Offset = w
+
+	p.Add(barsA, barsB, barsC)
+	p.Legend.Add("Group A", barsA)
+	p.Legend.Add("Group B", barsB)
+	p.Legend.Add("Group C", barsC)
+	p.Legend.Top = true
+	p.NominalX("One", "Two", "Three", "Four", "Five")
+	wt, _ := p.WriterTo(5*vg.Inch, 3*vg.Inch, "png")
+	//有其他接口调用的方式
+	wt.WriteTo(c.Writer)
+	//	第二种显示图片方式
+	// c.Header("Content-Type", "text/html; charset=utf-8")
+	// img := `<img src="data:image/png;base64,`
+	// buff := &bytes.Buffer{}
+	// wt.WriteTo(buff)
+	// base64.NewEncoder(base64.StdEncoding, buff).Write(buff.Bytes())
+	// png := base64.StdEncoding.EncodeToString(buff.Bytes())
+	// c.Writer.Write([]byte(img + png + `"/>`))
+	// if err := p.Save(5*vg.Inch, 3*vg.Inch, "barchart.png"); err != nil {
+	// 	panic(err)
+	// }
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/gin-contrib/sessions v0.0.1
 	github.com/gin-gonic/gin v1.4.0
+	gonum.org/v1/plot v0.0.0-20191004082913-159cd04f920c
 )
