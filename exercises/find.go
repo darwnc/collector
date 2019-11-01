50688f42700b18357c7e906533f85a998b61c45e
qty <qty1yure@gmail.com>
2019-11-01 10:37:54 +0800

    match any length string

--- exercises/find.go
+++ exercises/find.go
@@ -1,7 +1,6 @@
 package exercises
 
 import (
-	"fmt"
 	"strings"
 	"unicode/utf8"
 )
@@ -22,7 +21,13 @@ func findSubstring(s string, words []string) []int {
 	matchString := strings.Join(words, "")
 	matchStringLen := utf8.RuneCountInString(matchString)
 	r := []rune(s)
-	count := 0
+	// count := 0
+	source := make(map[int]string, len(words))
+
+	for k, v := range words {
+		source[k] = v
+	}
+
 	for k, v := range r {
 		if len(r)-k < matchStringLen {
 			break
@@ -33,59 +38,80 @@ func findSubstring(s string, words []string) []int {
 
 		// wordsCopy := make([]string, len(words))
 		// wordsCopy = wordsCopy[0:len(words)]
-		wordsCopy := make([]string, len(words))
-		copy(wordsCopy, words)
+		rest(source, words)
+		// wordsCopy := make([]string, len(words))
+		// copy(wordsCopy, words)
 		// fmt.Println(wordsCopy)
 		moveTo := 0
 		for {
 			if k+moveTo >= len(r) || len(r)-k < matchStringLen { //剩余的长度不够真个字符串的长度，则不符合
 				break
 			}
-			count++
+			// count++
 			// value = string(r[k+moveTo])
-			v = r[k+moveTo]
+			// v = r[k+moveTo]
 			// fmt.Println("value=", value)
 			// fmt.Println("word=", wordsCopy)
 			// fmt.Println("moveTo=", moveTo)
 			have := false
-			for i := 0; i < len(wordsCopy); i++ {
-				wordRune := []rune(wordsCopy[i])
+			for sk, sv := range source {
+				wordRune := []rune(sv)
 				if len(wordRune)+k+moveTo > len(r) {
 					break
 				}
+				v = r[k+moveTo]
 				// fmt.Println("length==", string(wordRune[0]))
 				// fmt.Println("wordsCopy==", wordsCopy[i])
 				if wordRune[0] == v { //第一个字符相等 跳过[i]字符验证下一组
 					wordLen := len(wordRune)
-					if string(r[k+moveTo:wordLen+k+moveTo]) == wordsCopy[i] { //符合，下一个match的item
+					if string(r[k+moveTo:wordLen+k+moveTo]) == sv { //符合，下一个match的item
 						// 删除当前item进行下一次迭代 ，下一次匹配的是当前s[k:len(words[i])+k+1]的值
 						moveTo += wordLen
-						wordsCopy = append(wordsCopy[:i], wordsCopy[i+1:]...)
+						delete(source, sk)
+						// sv = true
+						// wordsCopy = append(wordsCopy[:i], wordsCopy[i+1:]...)
 						// fmt.Println("substring s=", s[k:len(words[i])+k])
 						// fmt.Println("match=", words[i])
 						have = true
-						break
+						// break
 					}
 				}
 			}
 			//如果wordsCopy无变化，则当前不匹配，跳出循环
-			if len(wordsCopy) == len(words) || !have {
-				wordsCopy = nil
+			if len(source) == len(words) || !have {
+				// wordsCopy = nil
 				break
 			}
-			if len(wordsCopy) == 0 { //全符合
+			if len(source) == 0 { //全符合
 				// 记录index
 				result = append(result, k)
 				break
 			}
 		}
-		count++
+		// count++
 	}
-	fmt.Println("count=", count)
+	// fmt.Println("count=", count)
 	return result
 }
 
-func match(source []string, first string) bool {
-	fullString := strings.Join(source, "")
-	return strings.Contains(fullString, first)
+type staisfy struct {
+	word string
+	hit  bool
+}
+
+func find(source map[int]*staisfy) bool {
+	result := true
+	for _, v := range source {
+		if !v.hit {
+			result = false
+			break
+		}
+	}
+	return result
+}
+
+func rest(source map[int]string, words []string) {
+	for k, v := range words {
+		source[k] = v
+	}
 }
--- exercises/find_test.go
+++ exercises/find_test.go
@@ -6,7 +6,9 @@ import (
 )
 
 func TestFindString(t *testing.T) {
-	result := findSubstring("barfoothefoobarman", []string{"foo", "bar"})
+	// "barfoofoobarthefoobarman"
+	// ["bar","foo","the"]
+	result := findSubstring("barfoofoo我barthe我foobarman", []string{"bar", "foo我", "the我"})
 	// fullString := strings.Join([]string{"1", "12"}, "")
 	fmt.Println("result=", result)
 }
